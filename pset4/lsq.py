a896ff4c04d53f05359326034582f3c709e64e15
kai0511 <zhaokai09@gmail.com>
2017-06-12 15:40:31 +0800

    rearrage program files

---
+++ pset4/lsq.py
@@ -0,0 +1,21 @@
+def generate_models(x, y, degs):
+    """
+    Generate regression models by fitting a polynomial for each degree in degs
+    to points (x, y).
+    Args:
+        x: a list with length N, representing the x-coords of N sample points
+        y: a list with length N, representing the y-coords of N sample points
+        degs: a list of degrees of the fitting polynomial
+    Returns:
+        a list of numpy arrays, where each array is a 1-d array of coefficients
+        that minimizes the squared error of the fitting polynomial
+    """
+    import numpy as np
+    rslt = []
+    for deg in degs:
+        coefs = np.polyfit(x, y, deg)
+        rslt.append(coefs)
+    return rslt
+
+
+print(generate_models([1961, 1962, 1963],[4.4,5.5,6.6],[1, 2]))
